855ee5dc20ccc1c0e64088a27705463c5daeeb7b
Mario Antonio Sesso Junior <sessojunior@gmail.com>
2024-09-11 10:22:58 -0300

    Updated description of draw image

--- project-53-draw-image/src/App.jsx
+++ project-53-draw-image/src/App.jsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <>
       <h1>Draw image</h1>
-      <p>Projeto para desenhar em cima de uma imagem usando o mouse. É interessante para uso com uma mesa digitalizadora.</p>
+      <p>Projeto para desenhar em cima de uma imagem usando o mouse ou digitar em cima da imagem.</p>
       <DrawImage />
     </>
   )
