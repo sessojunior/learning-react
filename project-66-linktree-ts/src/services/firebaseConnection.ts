7ca2e759e07063f0a924936816a9b3df790068df
Mario Antonio Sesso Junior <sessojunior@gmail.com>
2024-10-04 16:36:17 -0300

    Update env variables

--- project-66-linktree-ts/src/services/firebaseConnection.ts
+++ project-66-linktree-ts/src/services/firebaseConnection.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore"
 import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
-	apiKey: "AIzaSyDUdNywr-4WEJ_07bQFtcXsoSSpr0lwMYo",
-	authDomain: "linktree-c20b9.firebaseapp.com",
-	projectId: "linktree-c20b9",
-	storageBucket: "linktree-c20b9.appspot.com",
-	messagingSenderId: "863031002962",
-	appId: "1:863031002962:web:6d29b7e278cef0061a9c8b",
+	apiKey: import.meta.env.VITE_APIKEY,
+	authDomain: import.meta.env.VITE_AUTHDOMAIN,
+	projectId: import.meta.env.VITE_PROJECTID,
+	storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+	messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+	appId: import.meta.env.VITE_APPID,
 }
 
 const app = initializeApp(firebaseConfig)
